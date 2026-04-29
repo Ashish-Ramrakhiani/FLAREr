66d3a1ddc187bde921a8ba661736c29e6d0decaf
Ashish-Ramrakhiani <ramrakhiyaniashish@gmail.com>
2026-04-28 22:56:31 -0700

    Wire flare_io.R into package: drop FaaSr Import, add aws.s3, export flare_*

--- R/globals.R
+++ R/globals.R
@@ -13,4 +13,6 @@ utils::globalVariables(c(".n", "AirTemp", "FLOW", "LongWave", "Rain", "RelHum", 
                          "variable", "variable_type", "wind_speed", "write.csv",
                          "lake_directory","multi_depth", "set_names", "force_tz", "as_vector", "pub_datetime", "log_weight", "reference_date",
                          "na.omit", "max_datetime", "reference_date", "reference_date", "obs_config", "multi_depth",
-                         "secchi_index", "working_directory", ".faasr"))
+                         "secchi_index", "working_directory", ".faasr",
+                         "faasr_get_file", "faasr_put_file", "faasr_delete_file",
+                         "faasr_get_folder_list", "faasr_arrow_s3_bucket"))
